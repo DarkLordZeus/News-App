ea6e4b93d1aae3bb8bd8c9031c1ee9961bc788dd
DarkLordZeus <92520238+DarkLordZeus@users.noreply.github.com>
2022-05-10 20:26:27 +0530

    News-App

--- app/src/main/java/com/example/newsapp/util/Constants.kt
+++ app/src/main/java/com/example/newsapp/util/Constants.kt
@@ -5,7 +5,7 @@ class Constants {
 
         const val BASE_URL="https://newsapi.org/"
         const val TIME_DELAY=500L
-        const val  API_KEY="yourapikeyhere"
+        const val  API_KEY="yourapikeyhereeeeeee"
         const val QUERY_PAGE_SIZE = 20
     }
 }
